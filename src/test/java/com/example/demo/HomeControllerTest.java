f9593b4f36b9ae9fa0bdde6ae45930a7cb68fdae
islam30577 <islam30577@gmail.com>
2026-01-27 14:18:08 +0300

    dev2 - 4 final

--- src/test/java/com/example/demo/HomeControllerTest.java
+++ src/test/java/com/example/demo/HomeControllerTest.java
@@ -8,10 +8,12 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
-@WebMvcTest(HomeController.class)
+@SpringBootTest
+@AutoConfigureMockMvc
 public class HomeControllerTest {
     
     @Autowired
